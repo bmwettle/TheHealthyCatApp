a6689d3dadf87991bf5599e0e9377e05d1526b47
bhayes804 <bhayes804@users.noreply.github.com>
2021-02-02 10:37:24 -0500

    Action 14 Configure app to initially open on Cat Settings activity for user to enter and save cat name and initial target weight

--- app/src/main/java/wsu/group18/thehealthycat/MainActivity.java
+++ app/src/main/java/wsu/group18/thehealthycat/MainActivity.java
@@ -1,11 +1,17 @@
 package wsu.group18.thehealthycat;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
     public Cat cat;
 
+    public String ChartButton = "Chart";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean hasStarted = prefs.getBoolean("hasStarted", true);
+
+        if (hasStarted) {
+            showStartupDialog();
+        }
+
         initializeCatClass();
     }
 
@@ -45,5 +61,39 @@ public class MainActivity extends AppCompatActivity {
         //The following is hard coded values for test purposes:
         List l = new ArrayList<>();
         cat = new Cat("Fred", 7.0, 10.0, l);
+    }
+
+    private void showStartupDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View startupDialogView = inflater.inflate(R.layout.startup_dialog, null);
+        builder.setTitle("Welcome to the Healthy Cat!");
+
+        final EditText catNameInput = (EditText) startupDialogView.findViewById(R.id.cat_name);
+        final EditText targetWeightInput = (EditText) startupDialogView.findViewById(R.id.target_weight);
+        builder.setView(startupDialogView);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TO-DO Save Input name and target weight to Cat object
+
+                //catName = catNameInput.getText().toString();
+                //targetWeight = targetWeightInput.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("hasStarted", false);
+        editor.apply();
     }
 }
